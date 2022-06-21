90dd254e3c91766e681f2f30b13439598765f873
shurjoMukhiDev <shurjomukhidev@gmail.com>
2022-06-21 16:19:25 +0600

    Project create and initialization

--- app/src/main/java/com/rzrasel/rztutorial/repository/AuthRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.rzrasel.rztutorial.repository
-
-import com.rzrasel.rztutorial.network.RetrofitApi
-
-class AuthRepository(private val api: RetrofitApi): BaseRepository() {
-    suspend fun login(email: String, password: String) = retrofitApiCall {
-        api.login(email, password)
-    }
-}
